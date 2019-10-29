0f678f8189ea3727890a7e41727b6177ba32dafd
Maven <omoakin1fe@gmail.com>
2019-10-29 13:56:32 +0100

    End of Section 14

--- MavenDate.API/Data/IDatingRepository.cs
+++ MavenDate.API/Data/IDatingRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using MavenDate.API.Helpers;
 using MavenDate.API.Models;
 
 namespace MavenDate.API.Data
@@ -9,7 +10,11 @@ namespace MavenDate.API.Data
          void Add<T>(T entity) where T: class;
          void Delete<T>(T entity) where T: class;
          Task<bool> SaveAll();
-         Task<IEnumerable<User>> GetUsers();
+
+         //WITHOUT PAGINATION
+        //  Task<IEnumerable<User>> GetUsers();
+         Task<PagedList<User>> GetUsers(UserParams userParams);
+
          Task<User> GetUser(int id);
          Task<Photo> GetPhoto(int id);
          Task<Photo> GetMainPhtoForUser(int userId);
